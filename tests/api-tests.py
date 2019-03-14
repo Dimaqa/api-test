0b32dae6105275ef5f99c682c6550ff27e2bb173
Dimaqa <best2010a@ya.ru>
2019-03-14 20:54:37 +0300

    init with posgres docker and test

---
+++ tests/api-tests.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+import requests
+
+URL = 'http://127.0.0.1:8080/{post_method}'
+
+
+class ApiTests(unittest.TestCase):
+    """Testing our api by sending different posts"""
+
+    def test_add_company(self):
+        url = URL.format(post_method='add_company')
+        valid_data = json.dumps({'company': 'intel'}).encode('utf-8')
+        code = requests.post(url, valid_data).status_code
+        self.assertEqual(code, 200)
