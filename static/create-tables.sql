d917d173a434c3918ea5baf2fb0e33aae50b0dd2
Dimaqa <best2010a@ya.ru>
2019-03-14 21:54:21 +0300

    create db structure

---
+++ static/create-tables.sql
@@ -0,0 +1,21 @@
+create Table companies(
+	id serial primary key,
+	name varchar(64) not null unique,
+	phone varchar(64)
+);
+create Table workers(
+	id serial primary key,
+	name varchar(64) not null unique,
+	phone varchar(64)
+);
+create table products(
+	id serial primary key,
+	name varchar(64) NOT Null unique
+);
+create Table connection(
+	worker_id serial Not null,
+	product_id serial Not null,
+	primary key(worker_id, product_id),
+	foreign key(worker_id) References workers(id),
+	foreign key(product_id) References products(id)
+);
