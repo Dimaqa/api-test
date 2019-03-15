d58737043da7a3528f07e015a5f6182f5634c641
Dmitry Shchedrov <shchedrov@sbtcom.ru>
2019-03-15 15:18:13 +0300

    tables to another file

--- app/db.py
+++ app/db.py
@@ -1,45 +1,13 @@
 import asyncpgsa
 from asyncpg.exceptions import UniqueViolationError, ForeignKeyViolationError
-from sqlalchemy import (
-    MetaData, Table, Column, ForeignKey,
-    Integer, String, DateTime
-)
-from sqlalchemy.sql import (select, update)
+from sqlalchemy.sql import select, update
+from db_tables import companies, workers, connection, products
 
-metadata = MetaData()
-
-companies = Table(
-    'companies', metadata,
-
-    Column('id', Integer, primary_key=True),
-    Column('name', String(64), nullable=False, unique=True),
-    Column('phone', String(64), nullable=True)
-)
-
-workers = Table(
-    'workers', metadata,
-
-    Column('id', Integer, primary_key=True),
-    Column('name', String(64), nullable=False, unique=True),
-    Column('company', String(64), nullable=True),
-    Column('phone', String(64), nullable=True)
-)
-products = Table(
-    'products', metadata,
-
-    Column('id', Integer, primary_key=True),
-    Column('name', String(64), nullable=False, unique=True),
-)
-connection = Table(
-    'connection', metadata,
-    Column('worker_id', Integer, ForeignKey('workers.id')),
-    Column('porduct_id', Integer, ForeignKey('product.id'))
-)
 
 async def init_db(app):
     pool = await asyncpgsa.create_pool(
         host='localhost',
-        port='5432',
+        port='5433',
         database='postgres',
         user='postgres',
         password='postgress',
@@ -79,6 +47,7 @@ async def insert_worker(conn, name, company):
             return 'There is no such company'
     return None
 
+
 async def edit_responsible(conn, product_id, worker_id):
     stmt = connection.insert().values(product_id=product_id, worker_id=worker_id)
     try:
@@ -91,10 +60,13 @@ async def edit_responsible(conn, product_id, worker_id):
         except ForeignKeyViolationError:
             return 'There is worker or product with such id'
     #return current resposible list with (product : name) format
-    stmt = select(
-        [products.c.name, workers.c.name.label('uname')]).\
-        select_from(products. \
-        outerjoin(connection). \
-        outerjoin(workers))
-    records = await conn.fetch(stmt)
-    return records
+    # stmt = select(
+    #     [products.c.name, workers.c.name.label('uname')]).\
+    #     select_from(products.
+    #                 outerjoin(connection).
+    #                 outerjoin(workers))
+    # records = {}
+    # for row in await conn.fetch(stmt):
+    #     records[row.name] = row.uname
+    # return records
+    return {'ok' : 'ok'}
---
+++ app/db_tables.py
@@ -0,0 +1,37 @@
+from sqlalchemy import (
+    MetaData, Table, Column, ForeignKey,
+    Integer, String, DateTime
+)
+
+metadata = MetaData()
+
+connection = Table(
+    'connection', metadata,
+
+    Column('worker_id', Integer, ForeignKey('workers.id')),
+    Column('product_id', Integer, ForeignKey('product.id'))
+)
+
+companies = Table(
+    'companies', metadata,
+
+    Column('id', Integer, primary_key=True),
+    Column('name', String(64), nullable=False, unique=True),
+    Column('phone', String(64), nullable=True)
+)
+
+workers = Table(
+    'workers', metadata,
+
+    Column('id', Integer, primary_key=True),
+    Column('name', String(64), nullable=False, unique=True),
+    Column('company', String(64), nullable=True),
+    Column('phone', String(64), nullable=True),
+)
+products = Table(
+    'products', metadata,
+
+    Column('id', Integer, primary_key=True),
+    Column('name', String(64), nullable=False, unique=True),
+)
+
